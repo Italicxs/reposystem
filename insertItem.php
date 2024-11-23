1c807e8458c9969a9e1579a47744ea9c1379a49f
Emiliano Machado <73149930+Italicxs@users.noreply.github.com>
2024-11-22 22:19:12 -0700

    insertItem.php

--- insertItem.php
+++ insertItem.php
@@ -1,6 +1,6 @@
 <?php
-	require_once('../../inc/config/constants.php');
-	require_once('../../inc/config/db.php');
+	require_once'../../inc/config/constants.php';
+	require_once'../../inc/config/db.php';
 	
 	$initialStock = 0;
 	$baseImageFolder = '../../data/item_images/';

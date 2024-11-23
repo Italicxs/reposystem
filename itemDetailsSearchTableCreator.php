6bdfa010b7e417be76007f184988533fce7480fb
Emiliano Machado <73149930+Italicxs@users.noreply.github.com>
2024-11-22 22:20:25 -0700

    itemDetailsSearchTableCreator.php

--- itemDetailsSearchTableCreator.php
+++ itemDetailsSearchTableCreator.php
@@ -1,6 +1,6 @@
 <?php
-	require_once('../../inc/config/constants.php');
-	require_once('../../inc/config/db.php');
+	require_once'../../inc/config/constants.php';
+	require_once'../../inc/config/db.php';
 	
 	$itemDetailsSearchSql = 'SELECT * FROM item';
 	$itemDetailsSearchStatement = $conn->prepare($itemDetailsSearchSql);

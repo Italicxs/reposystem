1c06c0931fc2b1079d6291b2f451359588bfa100
Emiliano Machado <73149930+Italicxs@users.noreply.github.com>
2024-11-22 22:50:51 -0700

    updateSale.php

--- updateSale.php
+++ updateSale.php
@@ -1,7 +1,7 @@
 <?php
 
-	require_once('../../inc/config/constants.php');
-	require_once('../../inc/config/db.php');
+	require_once'../../inc/config/constants.php';
+	require_once'../../inc/config/db.php';
 	
 	if(isset($_POST['saleDetailsSaleID'])){
 

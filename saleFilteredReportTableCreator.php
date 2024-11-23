db8764172b1637147a19344d5f3471054e626a9e
Emiliano Machado <73149930+Italicxs@users.noreply.github.com>
2024-11-22 22:30:42 -0700

    saleFilteredReportTableCreator.php

--- saleFilteredReportTableCreator.php
+++ saleFilteredReportTableCreator.php
@@ -1,6 +1,6 @@
 <?php
-	require_once('../../inc/config/constants.php');
-	require_once('../../inc/config/db.php');
+	require_once'../../inc/config/constants.php';
+	require_once'../../inc/config/db.php';
 	
 	$uPrice = 0;
 	$qty = 0;

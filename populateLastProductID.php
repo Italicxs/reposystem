55c4ff9e0fd35ee43db6ce3fe147a1a9fbb906fb
Emiliano Machado <73149930+Italicxs@users.noreply.github.com>
2024-11-22 22:23:38 -0700

    populateLastProductID.php

--- populateLastProductID.php
+++ populateLastProductID.php
@@ -1,6 +1,6 @@
 <?php
-	require_once('../../inc/config/constants.php');
-	require_once('../../inc/config/db.php');
+	require_once'../../inc/config/constants.php';
+	require_once'../../inc/config/db.php';
 	
 	$sql = "SELECT MAX(productID) FROM item";
 	$stmt = $conn->prepare($sql);
